345db3dff038f063909c5b3a7dc4d34a92a772c5
Takashige257 <kolbii@mac.com>
2021-04-13 18:10:32 +0900

    Add files via upload
    pwa test

---
+++ sample/service_worker.js
@@ -0,0 +1,24 @@
+var CACHE_NAME = 'pwa-sample-caches';
+var urlsToCache = [
+	'/qE20gFKVrZ4fUZqIQMIR4KcozpmVWRYX/',
+	'/qE20gFKVrZ4fUZqIQMIR4KcozpmVWRYX/images/ic_launcher.png',
+	'/qE20gFKVrZ4fUZqIQMIR4KcozpmVWRYX/images/_IGP4305.jpg'
+];
+// インストール処理
+self.addEventListener('install', function(event)
+{
+	event.waitUntil(caches
+		.open(CACHE_NAME)
+		.then(function(cache)
+		{
+			return cache.addAll(urlsToCache);
+		}));
+});
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event)
+{
+	event.respondWith(caches.match(event.request).then(function(response)
+	{
+		return response ? response : fetch(event.request);
+	}));
+});
